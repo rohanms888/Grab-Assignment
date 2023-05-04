d0e3ea87fbfd38e18c0fce04bd457f3fbbb96e7d
rohanms888 <88832105+rohanms888@users.noreply.github.com>
2023-05-04 17:41:47 +0530

    Update Grab.py

--- Grab.py
+++ Grab.py
@@ -4,18 +4,18 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Set up the Chrome driver
+
 options = webdriver.ChromeOptions()
 options.add_argument('--incognito')
 driver = webdriver.Chrome(options=options)
 
-# Navigate to the Grab food page
+
 driver.get('https://food.grab.com/ph/en/')
 
-# Wait for the page to load
+
 time.sleep(5)
 
-# Find the "Load more" button and click it until all restaurants are displayed
+
 load_more_button = driver.find_element(By.XPATH, "//button[@data-test-id='load-more-button']")
 while load_more_button.is_displayed():
     action = ActionChains(driver)
@@ -23,17 +23,17 @@ while load_more_button.is_displayed():
     load_more_button.click()
     time.sleep(2)
 
-# Find all the restaurant elements
+
 restaurant_elements = driver.find_elements(By.XPATH, "//div[@class='cXIPh _3O_kR']")
 
-# Loop through each restaurant element and extract the latitude and longitude
+
 for restaurant in restaurant_elements:
     try:
-        # Hover over the restaurant element to reveal the hidden latitude and longitude
+        
         action = ActionChains(driver)
         action.move_to_element(restaurant).perform()
 
-        # Extract the latitude and longitude from the tooltip
+        
         tooltip = driver.find_element(By.XPATH, "//div[@class='_2ZQhq']")
         latitude, longitude = tooltip.get_attribute('innerHTML').split(',')
 
@@ -41,5 +41,5 @@ for restaurant in restaurant_elements:
     except:
         continue
 
-# Close the browser window
+
 driver.quit()
